b3d958113ce5c9e3b8f65a9e6e408b4524576eb2
lovro <lovro.zagar5@gmail.com>
2022-12-10 18:46:28 +0100

    Add form functionality, add and stlye main container

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict"
+
+const password = document.querySelector('#password');
+const confirmedPassword = document.querySelector('#confirm-password');
+const errorAlert = document.querySelector('#pass');
+
+
+confirmedPassword.addEventListener('input', () => {
+    if (confirmedPassword.value != null && confirmedPassword.value !== password.value) {
+        password.style.border = "1.5px solid #C54242";
+        confirmedPassword.style.border = "1.5px solid #C54242";
+        errorAlert.classList.add('no-match');
+        return;
+    }
+    password.style.border = "1.5px solid #E7E9EC";
+    confirmedPassword.style.border = "1.5px solid #E7E9EC";
+    errorAlert.classList.remove('no-match');
+});
